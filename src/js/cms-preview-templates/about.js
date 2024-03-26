7eb0fe464d23dec5ed15e7c886e25855b87aad76
Sumögåsbord <sumogasbord@gmail.com>
2024-03-26 23:12:04 +0900

    Update about.js

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -13,9 +13,9 @@ export default class AboutPreview extends React.Component {
       <div className="bg-off-white pv4">
         <div className="ph3 mw7 center">
           <div className="flex-ns flex-wrap mhn2-ns mb3">
-            {(entry.getIn(["data", "intro", "blurbs"]) || []).map((blurb, index) => <div className="ph2-ns w-50-ns mb4" key={index}>
+            {(entry.getIn(["data", "body"]) || []).map((blurb, index) => <div className="ph2-ns w-50-ns mb4" key={index}>
               <img src={blurb.get("image") && getAsset(blurb.get("image"))} alt="" className="center db mb3" style={{width: "240px"}}/>
-              <p>{blurb.get("text")}</p>
+              <p>{blurb.get("body")}</p>
             </div>)}
           </div>
         </div>
